d7a118b5d71b4684931ee4dc081c36cf3d4cf239
lolitthax <aline.isilvadamiao@gmail.com>
2020-05-01 14:18:44 -0300

    alteração na function.js
    agora os elementos desaparecem qnd você volta ao topo e a função é ativada enquanto você está na página, não somente ao visitar a página pela primeira vez ou atualizá-la ♥

--- js/function.js
+++ js/function.js
@@ -1,3 +1,19 @@
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+        const context = this;
+        const later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+
 const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
@@ -6,12 +22,17 @@ function animeScroll() {
     target.forEach(function(element) {
         if ((windowTop) > element.offsetTop) {
             element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
         }
-
-        console.log(element.offsetTop);
     })
 }
 
-window.addEventListener('scroll', function() {
-    animeScroll();
-})
+animeScroll();
+
+if (target.length) {
+    window.addEventListener('scroll', debounce(function() {
+        animeScroll();
+        console.log
+    }, 200))
+}
